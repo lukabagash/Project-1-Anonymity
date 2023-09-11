13530914458b1fd78450f252076e6f9f9ca8233f
Luka <luka.yep@gmail.com>
2023-09-11 16:56:37 -0400

    attribute supression added

--- code/k_anonymity.py
+++ code/k_anonymity.py
@@ -13,13 +13,25 @@ class Anonymization:
         return pd.read_csv(data_path, encoding="ISO-8859-1")
 
 
+    def suppress_attributes(self, columns_to_suppress):
+        """
+        Suppress the specified columns from the dataset.
+        """
+        self.anonymized_data = self.data.drop(columns=columns_to_suppress, errors='ignore')
+
     def anonymize(self, k):
         """
         Anonymize the data for a given k value.
         This is a placeholder and needs the actual anonymization logic.
         """
         # TODO: Implement the actual anonymization logic here
+        columns_to_suppress = ["Passenger ID", "First Name", "Last Name", "Nationality", "Airport Name", "Airport Country Code", "Country Name", "Continents", "Pilot Name"]
         self.anonymized_data = self.data.copy()  # Placeholder: currently just copying the original data
+        # Suppress the specified columns
+
+        if columns_to_suppress:
+            self.suppress_attributes(columns_to_suppress)
+
 
     def save_to_csv(self, filename):
         """
